f4613842c62c29abbf35f5a9951e471c84da08ed
Rc <Rc@Rc>
2020-04-16 13:02:54 +0530

    Java8 features- Demoforeach

---
+++ Java8_features/src/eightfeatures/Demo_foreach.java
@@ -0,0 +1,53 @@
+package eightfeatures;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+/*class ConsumerImpl implements Consumer<Integer>
+{
+
+	@Override
+	public void accept(Integer t) {
+		System.out.println(t);
+	}
+	
+}*/
+
+public class Demo_foreach {
+
+	public static void main(String[] args) {
+		List<Integer> values=Arrays.asList(1,2,3,4,5);
+	
+		/*	Different way of using foreach with condition
+		 values.forEach(
+				i->
+				{
+					if(i>3)
+					System.out.println(i);
+				}
+				);*/
+		
+		
+		/*Consumer<Integer> c=new ConsumerImpl();
+		values.forEach(c);*/
+		
+		
+		/*Consumer<Integer> c=new Consumer<Integer>()
+				{
+					@Override
+					public void accept(Integer t) {
+						System.out.println(t);
+					}
+				};
+				
+		 values.forEach(c);*/
+		
+	  /*	Consumer<Integer> c= (i)->System.out.println(i);
+		values.forEach(c);*/
+		
+		values.forEach(i->System.out.println(i));
+		
+		
+	}
+
+}
